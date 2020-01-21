ce0a2e44330f90c3cbd22c0b8afbb876de15341e
Ivan Shevchenko <rayserviz@gmail.com>
2020-01-21 18:50:36 +0200

    start hw

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import user from '../../database/user.json';
+
+console.log(user)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './components/Profile/Profile';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
