6a721e1aec8f8b5e9e390d9f77313376068968f8
Ivan Shevchenko <rayserviz@gmail.com>
2020-01-25 13:43:49 +0200

    start task 02

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import userData from '../database/user.json';
+import statData from '../database/statistical-data.json';
 
 export default function App() {
   return (
     <>
       <Profile user={userData} />
+      <Statistics title="Upload Stats" data={statData} />
     </>
   );
 }
